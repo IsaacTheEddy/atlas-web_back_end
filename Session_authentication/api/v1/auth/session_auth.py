1bc31c6b9eb9a3e7831e30eed640cf1cb83d5d0b
Dtbissy <isaactheedwards@gmail.com>
2025-01-29 17:43:01 -0600

    Redundant backslasj

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -23,7 +23,7 @@ class SessionAuth(Auth):
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
-    def user_id_for_session_id(self, \
+    def user_id_for_session_id(self,
                                session_id: str = None) -> str:
         """Returns User Id based on Session Id"""
         if session_id is None or not isinstance(session_id, str):
