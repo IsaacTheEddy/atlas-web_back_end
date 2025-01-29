256c74352f0bc8b60b1261f89bc72c60434d3768
Dtbissy <isaactheedwards@gmail.com>
2025-01-29 17:31:29 -0600

    Addded session create function using uuid

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,22 @@
 class"""
 from api.v1.auth.auth import Auth
 import os
-
+import uuid
 
 class SessionAuth(Auth):
     """Session Auth class that inheirts
     from Auth class"""
     def __init__(self):
         super().__init__()
-        pass
+
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session type session"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        else:
+            session_id = str(uuid.uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
