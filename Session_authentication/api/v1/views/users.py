85dd0a78d06d3ac5e3bed326de37c64c010958ba
Dtbissy <isaactheedwards@gmail.com>
2025-01-29 20:31:19 -0600

    Might have had a redundant route

--- Session_authentication/api/v1/views/users.py
+++ Session_authentication/api/v1/views/users.py
@@ -15,17 +15,6 @@ def view_all_users() -> str:
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def view_me()-> str:
-    """Get /api/v1/users
-    Return:
-    - list of me
-    """
-    if request.current_user is None:
-        abort(404)
-    return jsonify(request.current_user.to_json())
-
-
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
     """ GET /api/v1/users/:id
