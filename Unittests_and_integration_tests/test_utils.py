78180b0a79474807eb767cb98ba3e1eaeed0453a
Dtbissy <isaactheedwards@gmail.com>
2025-02-04 12:59:51 -0600

    Addded memozation test class

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """This is my test module for Utils.py"""
 import unittest
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 from parameterized import parameterized
 import json
 
@@ -22,7 +22,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """This will test access.tested_maps methodfrom utils.py"""
         self.assertEqual(access_nested_map(nested_map=param1, path=param2), expectedResult)
 
-
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, {"a", "b"})
@@ -32,6 +31,7 @@ class TestAccessNestedMap(unittest.TestCase):
         access method Class"""
         with self.assertRaises(expected_exception=KeyError):
             access_nested_map(nested_map=param1, path=param2)
+
 
 class TestGetJson(unittest.TestCase):
     """Test class is used for Utils.py GetJson Cases"""
@@ -49,3 +49,27 @@ class TestGetJson(unittest.TestCase):
             mock_get.return_value = mock
             self.assertEqual(get_json(param), expectedResult)
             mock_get.assert_called_once_with(param)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test class is used to mock and test the memoize method
+    in Utils.py"""
+
+    def test_memoize(self):
+        """Test to mock a memoize method"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_a:
+            test_class = TestClass()
+            run = test_class.a_property
+            run1 = test_class.a_property
+            self.assertEqual(run1, 42)
+            self.assertEqual(run, 42)
+            mock_a.assert_called_once()
