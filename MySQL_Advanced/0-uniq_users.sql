0a336940483aad2836741f3ed5af55b13260e4eb
Dtbissy <isaactheedwards@gmail.com>
2025-02-08 14:15:33 -0600

    Moved Unique. I spnt an hour figuruing out how to change root passwords in unix. If this is all then what a great time i've had

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,7 @@
 -- name
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY(id),
-    UNIQUE(email)
 )
