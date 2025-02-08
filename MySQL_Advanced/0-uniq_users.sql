944d970eb61747ad3a17364b65b859a5801fbaa9
Dtbissy <isaactheedwards@gmail.com>
2025-02-08 13:17:56 -0600

    Added comments

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+-- Creates table users
+-- id - in not null and auto increments pk
+--email - not null unique key
+-- name
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
