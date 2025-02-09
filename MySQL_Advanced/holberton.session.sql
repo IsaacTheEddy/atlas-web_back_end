74b7eb39a5463d5d0c3b130cd14bf9a6ce4ea1ef
Dtbissy <isaactheedwards@gmail.com>
2025-02-09 01:04:40 -0600

    Added task one for create a table with a country that enumerates;

--- MySQL_Advanced/holberton.session.sql
+++ MySQL_Advanced/holberton.session.sql
@@ -1,7 +1,7 @@
+-- Stuff an things
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    name varchar(255),
-    PRIMARY KEY(id),
-    UNIQUE(email)
-)
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
