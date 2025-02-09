98e10b150dd2439ed4f5715e462aaf46e544eb03
Dtbissy <isaactheedwards@gmail.com>
2025-02-09 01:18:22 -0600

    Boom

--- MySQL_Advanced/holberton.session.sql
+++ MySQL_Advanced/holberton.session.sql
@@ -1,7 +1,6 @@
--- Stuff an things
+-- Creates table users
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
-);
+    email varchar(255) NOT NULL UNIQUE,
+    name varchar(255),
+)
