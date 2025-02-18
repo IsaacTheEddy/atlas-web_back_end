465015ce19cd77caed39c56be15899f125e11d6d
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 17:14:50 -0600

    Trigger triggers the database to update everytime we insert data into orders

---
+++ MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Updates each row with a trigger
+CREATE TRIGGER decrease
+AFTER
+INSERT ON orders FOR EACH ROW BEGIN
+UPDATE items
+SET quantity = quantity - NEW.quantity
+WHERE name = NEW.item_name;
+END;
