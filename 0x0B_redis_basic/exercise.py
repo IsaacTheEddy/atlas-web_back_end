d4d90ef32c0702fc10c6754906c5869cd8a07f75
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 14:11:34 -0600

    Added a functools wrapper method to keep count of the calls made to store

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 import redis
 import uuid
+from functools import wraps
 from typing import Union, Callable, Optional
+
+
+def count_calls(method: Callable) -> callable:
+    """
+    Will keep track of how many calls are made to
+    the cache class
+
+    Args:
+    method (Callable): the wrapper that keeps count
+
+    Return:
+    Callable: Calls the function over and over again.
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,6 +34,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store data in Redis with a randomly genereated key.
@@ -28,7 +51,16 @@ class Cache:
 
     def get(self, key: str, fn: Optional[Callable[[bytes], Union[str, int]]] = None
                 ) -> Optional[Union[str, int]]:
-        """ Retrieve data from Redis and optionally apply a conversion function.
+        """
+        Retrieve data from Redis and optionally apply a conversion function.
+
+        Args:
+        key (str) the key name for Redis
+        fn Optional[Callable[[bytes], Union[str, int]]] an optional function to be called
+        if the value passed isnt a string
+        Returns:
+        None
+
         """
         value = self._redis.get(key)
 
@@ -40,7 +72,27 @@ class Cache:
 
 
     def get_str(self, key: str) -> str:
+        """
+        Gets the String value from Redis
+
+        Args:
+        key (str) the name of the key
+
+        Returns:
+        str: the string representation of the key: value
+        """
         return self.get(key, lambda x: x.decode('utf-8'))
 
     def get_int(self, key: str) -> int:
-        return self.get(key, lambda x: int(x))
+            """
+        Gets the Int value from Redis
+
+        Args:
+        key (str) the name of the key
+
+        Returns:
+        int: the string representation of the key: value
+        """
+            return self.get(key, lambda x: int(x))
+
+
