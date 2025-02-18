608a26e49dbf8855548ab3eace257392d51a5f83
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 15:20:35 -0600

    I moved replay out pf the class

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -123,26 +123,23 @@ class Cache:
         """
             return self.get(key, lambda x: int(x))
 
-    def replay(method: Callable) -> None:
-        """
-        Displays all the data
+def replay(method: Callable) -> None:
+    """ Formats all teh data collection for display purposes
 
-        Args:
-        method (Callable) The function wrapped
+    Args:
+        method (Callable): Can take any method defined
+    """
 
-        """
-        method_name = method.__qualname__
-        count_key = method_name
-        inputs = f"{method.__qualname__}:inputs"
-        outputs = f"{method.__qualname__}:outputs"
+    method_name = method.__qualname__
+    count_key = method_name
+    inputs_key = f"{method_name}:inputs"
+    outputs_key = f"{method_name}:outputs"
 
-        count = method.__self__._redis.get(count_key)
-        input_list = method.__self__._redis.lrange(inputs, 0, -1)
-        output_list = method.__self__._redis.lrange(outputs, 0, -1)
+    count = method.__self__._redis.get(count_key)
+    inputs = method.__self__._redis.lrange(inputs_key, 0, -1)
+    outputs = method.__self__._redis.lrange(outputs_key, 0, -1)
 
-        print(f"{method_name} was called {int(count)} times:")
-        for input_str, output_str in zip(input_list, output_list):
-            print(f"{method_name}(*{input_str.decode('utf-8')}) ->\
-                  {output_str.decode('utf-8')}")
-
-
+    print(f"{method_name} was called {int(count)} times:")
+    for input_str, output_str in zip(inputs, outputs):
+        print(f"{method_name}(*{input_str.decode('utf-8')}) ->\
+            {output_str.decode('utf-8')}")
