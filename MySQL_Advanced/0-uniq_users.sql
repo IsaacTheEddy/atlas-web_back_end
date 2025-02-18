9ac4ebe28a263fadb9f96b64e5b75a54b966c352
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 15:29:59 -0600

    Another attempt

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,5 @@
--- Creates table users
-CREATE TABLE users (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
+    name VARCHAR(255)
 );
