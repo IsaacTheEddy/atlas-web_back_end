b090b4ef91b8b634aa082bddab502e778eab6470
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 15:28:28 -0600

    bRUH

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Creates table users
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
