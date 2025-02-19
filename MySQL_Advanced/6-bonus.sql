d21682329881ecc53d41b42acd181c6aaa56015e
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 20:40:48 -0600

    wtf

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,18 @@
 -- Creates a procedure to update a project score with user_id
-CREATE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-) BEGIN
-DECLARE project_id INT;
-SELECT id INTO project_id
-FROM projects
-WHERE name = project_name;
-IF project_id IS NULL THEN
-INSERT INTO projects (name)
-VALUES (project_name);
-set project_id = LAST_INSERT_ID();
-END IF;
-INSERT INTO corrections (user_id, project_id, score)
-VALUES (user_id, project_id, score, 'bonus');
-END;
+-- CREATE PROCEDURE AddBonus(
+--     IN user_id INT,
+--     IN project_name VARCHAR(255),
+--     IN score INT
+-- ) BEGIN
+-- DECLARE project_id INT;
+-- SELECT id INTO project_id
+-- FROM projects
+-- WHERE name = project_name;
+-- IF project_id IS NULL THEN
+-- INSERT INTO projects (name)
+-- VALUES (project_name);
+-- set project_id = LAST_INSERT_ID();
+-- END IF;
+-- INSERT INTO corrections (user_id, project_id, score)
+-- VALUES (user_id, project_id, score, 'bonus');
+-- END;
