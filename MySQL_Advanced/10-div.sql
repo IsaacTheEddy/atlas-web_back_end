0a4c652e644eb14a0a5582e653b26e9ee6e15566
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 21:18:36 -0600

    Changed things to a float

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,8 +1,4 @@
--- divided and returns the first numbe
-CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS DECIMAL(10, 2) -- Choose appropriate precision and scale
-DETERMINISTIC -- Important: Add DETERMINISTIC or READS SQL DATA
-BEGIN IF b = 0 THEN RETURN 0;
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN IF b = 0 THEN RETURN 0;
 ELSE RETURN a / b;
--- Or CAST(a AS DECIMAL(10,2)) / b for more control
 END IF;
 END;
