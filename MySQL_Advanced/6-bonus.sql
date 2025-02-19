56ddc81a03b0f978cf0c940d433cf13e89f5b893
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 20:24:22 -0600

    I dk lets see what this does

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,18 +1,21 @@
--- Creates a procedure to update a project score with user_id
-CREATE PROCEDURE AddBonus(
+DELIMITER // CREATE PROCEDURE AddBonus (
     IN user_id INT,
     IN project_name VARCHAR(255),
     IN score INT
 ) BEGIN
 DECLARE project_id INT;
+-- Check if the project exists
 SELECT id INTO project_id
 FROM projects
 WHERE name = project_name;
+-- If the project doesn't exist, insert a new project
 IF project_id IS NULL THEN
 INSERT INTO projects (name)
 VALUES (project_name);
-set project_id = LAST_INSERT_ID();
+-- Get the last inserted project ID
+SET project_id = LAST_INSERT_ID();
 END IF;
+-- Add the correction (linking user, project, and score)
 INSERT INTO corrections (user_id, project_id, score)
 VALUES (user_id, project_id, score);
-END;
+END // DELIMITER;
