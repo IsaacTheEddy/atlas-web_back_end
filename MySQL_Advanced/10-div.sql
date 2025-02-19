c3be587e5e2b5117520d8cdfc2327adfc4a5fcc2
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 21:10:42 -0600

    hmm

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,8 +1,8 @@
--- divided and returns the first number by the second number and returns
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT BEGIN
-DECLARE result FLOAT;
-IF b = 0 THEN RETURN 0;
-ELSE
-SET RETURN a / b;
+-- divided and returns the first numbe
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS DECIMAL(10, 2) -- Choose appropriate precision and scale
+DETERMINISTIC -- Important: Add DETERMINISTIC or READS SQL DATA
+BEGIN IF b = 0 THEN RETURN 0;
+ELSE RETURN a / b;
+-- Or CAST(a AS DECIMAL(10,2)) / b for more control
 END IF;
-END;
+END
