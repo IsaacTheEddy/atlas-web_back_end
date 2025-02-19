1796e9e6877a6f45cf381727b828a6fbc8e50581
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 21:01:35 -0600

    A dividding function

---
+++ MySQL_Advanced/10-div.sql
@@ -0,0 +1,10 @@
+-- divided and returns the first number by the second number and returns
+CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT BEGIN
+DECLARE result FLOAT;
+IF b = 0 THEN
+SET result = 0;
+ELSE
+SET result = a / b;
+END IF;
+RETURN result;
+END;
