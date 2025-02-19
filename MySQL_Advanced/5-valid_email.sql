e8f8efd97739c3beeb53253d68995af43573b8c7
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 20:05:14 -0600

    Fianlly managed to get the database to update based on score. It was really a probleme with initlaizing a procedure and then not dropping it after i made a correctiond

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,6 +1,6 @@
 -- Resets valid_email when email is changed
 CREATE TRIGGER reset_email BEFORE
-UPDATE ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
+UPDATE ON users FOR EACH ROW BEGIN If NEW.email <> OLD.email THEN
 SET NEW.valid_email = 0;
 END IF;
 END;
