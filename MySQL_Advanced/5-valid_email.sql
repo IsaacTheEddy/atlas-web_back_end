3b4ab2daffbf114bb227f35f1ab462476050029e
Dtbissy <isaactheedwards@gmail.com>
2025-02-18 18:36:06 -0600

    Lets see

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,6 @@
+-- Resets valid_email when email is changed
+CREATE TRIGGER reset_email BEFORE
+UPDATE ON users FOR EACH ROW BEGIN IF NEW.email <> OLD.email THEN
+SET NEW.valid_email = 0;
+END IF;
+END;
