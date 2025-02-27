ba7fd895bd5326b08fe070d9fb4568f221f8520b
Dtbissy <isaactheedwards@gmail.com>
2025-02-27 05:05:01 -0600

    Idk

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -1,14 +1,21 @@
 const sinon = require("sinon");
 const chai = require("chai")
-const assert = require('assert');
+const assert = require("assert")
 const Utils = require("./utils")
-const sendPaymentRequestToApi = require("./5-payment")
+const sendPaymentRequestToApi = require("./3-payment")
 const {expect} = chai
 
 describe("sendPaymentRequestToApi", () => {
-    it("Calls Utils.calculateNumbers with Type(SUM) and 100 and 20", ()=>{
-        const spy = sinon.spy(console, 'log');
+    let spy;
 
+    beforeEach(() => {
+        spy = sinon.spy(console, "log");
+    });
+
+    afterEach(() => {
+        spy.restore();
+    });
+    it("Calls Utils.calculateNumbers with Type(SUM) and 100 and 20", ()=>{
         sendPaymentRequestToApi(100, 20);
 
         assert.strictEqual(spy.callCount, 1);
@@ -17,8 +24,6 @@ describe("sendPaymentRequestToApi", () => {
         spy.restore();
     });
     it("Calls Utils.calculateNumbers with Type(SUM) and 10 and 10", ()=>{
-        const spy = sinon.spy(console, 'log');
-
         sendPaymentRequestToApi(10, 10);
 
         assert.strictEqual(spy.callCount, 1);
@@ -26,10 +31,4 @@ describe("sendPaymentRequestToApi", () => {
 
         spy.restore();
     });
-    // it("Calls Utils.calculateNumbers with Type(SUM) and 10 and 10", ()=>{
-    //     sendPaymentRequestToApi(10, 10);
-    //     console.log(`The result is: ${result}`)
-    //     expect(console.log.calledOnce).to.be.true
-    //     spy.restore();
-    // });
 });
