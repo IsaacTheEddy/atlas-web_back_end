5c7a350864208e35a27097cd999e3a9fad750006
Dtbissy <isaactheedwards@gmail.com>
2025-02-26 19:34:13 -0600

    typo

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -4,7 +4,7 @@ const calculateNumber = require('./0-calcul.js')
 
 describe('calculateNumber', () =>{
     it("Returns 4 if 1 and 3 are passed", function (){
-        assert.strictEqual(calculateNumber(1, 1), 2);
+        assert.strictEqual(calculateNumber(1, 3), 4);
     })
     it("Returns 5 if 1 and 3.7 are passed", function() {
         assert.strictEqual(calculateNumber(1, 3.7), 5)
