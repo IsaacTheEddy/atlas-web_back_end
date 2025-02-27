578266adf1ba1d32c1111c2db3cb9c73ae9d901a
Dtbissy <isaactheedwards@gmail.com>
2025-02-26 19:31:52 -0600

    I hate npm i dont like nvm its been a long ride just figureing out whcih version works

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
-const assert = require('assert')
-import { calculateNumber } from './0-calcul.js';
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js')
 
 
 describe('calculateNumber', () =>{
