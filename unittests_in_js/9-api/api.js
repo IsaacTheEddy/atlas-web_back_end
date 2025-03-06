d29d31a4d6e9d3029025167e7b43c055595ce3db
Dtbissy <isaactheedwards@gmail.com>
2025-03-06 00:00:16 -0600

    try agaun

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -1,18 +1,22 @@
-const express = require("express")
+const express = require('express');
+const app = express();
 
-const app = express()
-const port = 7865
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
 
-app.get("/", (req,res) => {
-    res.end("Welcome to the payment system")
-})
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  const regex = /^\d+$/;
+  if (!regex.test(id)) {
+    res.status(404).send('Invalid ID format');
+    return;
+  }
+  res.send(`Payment methods for cart ${id}`)
+});
 
-app.get("/cart/:id", (req,res) => {
-    if (id === !isNaN){
-        console.log("")
-    }
-    res.end("")
-})
-app.listen(port, () => {
-    console.log(`API available on localhost port ${port}`)
-})
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865')
+});
+
+module.exports = app;
